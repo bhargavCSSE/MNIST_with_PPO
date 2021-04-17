9b9011bfcbf15158be788b97396a2ca2cc613358
Bhargav Joshi <bhargav.csse@gmail.com>
2021-04-16 20:09:27 -0500

    CODE COMPLETE

--- main.py
+++ main.py
@@ -1,30 +1,95 @@
 import gym
+from gym import spaces
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from ppo_torch import Agent
 from matplotlib import pyplot as plt
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+
+class MNIST_trainer(gym.Env):
+    def __init__(self):
+        super(MNIST_trainer, self).__init__()
+        
+        # Environment params
+        self.action_space = spaces.Discrete(10)
+        self.observation_space = spaces.Box(low=0, high=255, shape=(8*8, ), dtype=np.int8)
+
+        # Environment data
+        mnist = load_digits()
+        self.images = mnist['images']
+        self.labels = mnist['target']
+        self.images = self.flatten(self.images)
+        self.images, self.images_test, self.labels, self.labels_test = train_test_split(self.images, self.labels, 
+                                                                                        test_size=0.2, train_size=0.8, 
+                                                                                        shuffle=True)
+        self.current_state = 0
+        self.end_state = len(self.labels) - 1
+
+        self.current_state_test = 0
+        self.end_state_test = len(self.labels_test) - 1
+
+    def step(self, action):
+        reward = 0
+        done = False
+        info = "MNIST-Trainer"
+        predicted_label = action
+        correct_label = self.labels[self.current_state]
+
+        if predicted_label == correct_label:
+            reward = 1
+        else:
+            reward = 0
+        
+        if self.current_state == self.end_state:
+            done = True
+
+        if done is False:
+            self.current_state += 1
+            next_state = self.images[self.current_state]
+        else:
+            next_state = -np.inf
+
+        return next_state, reward, done, info
+    
+    def reset(self):
+        self.current_state = 0
+        state = self.images[self.current_state]
+
+        return state
+
+    def render(self, mode='human', close=False):
+        pass
+
+    def flatten(self, images):
+        flatten_images = []
+        for i in range(images.shape[0]):
+            image = np.array(images[i])
+            flatten_images.append(image.flatten())
+        return np.array(flatten_images)
+
 
 if __name__ == "__main__":
 
     # Environment settings
-    env = gym.make('LunarLander-v2')    
+    env = MNIST_trainer() 
     
     # Trainer's settings
     load_checkpoint = False
     chkpt_dir = 'tmp/ppo'
     render = False
-    n_trials = 10
-    n_games = 100
+    n_trials = 5
+    n_episodes = 1000
 
     # PPO params
     N = 20
-    batch_size = 5
-    n_epochs = 4
-    alpha = 0.0005 
+    batch_size = 10
+    n_epochs = 3
+    alpha = 0.00003 
     best_score = env.reward_range[0]
-    layer_1_dim = 256
-    layer_2_dim = 256
+    layer_1_dim = 128
+    layer_2_dim = 128
 
     # Final results
     score_book = {}
@@ -54,7 +119,7 @@ if __name__ == "__main__":
         if load_checkpoint:
             agent.load_models()
 
-        for i in tqdm(range(n_games)):
+        for i in tqdm(range(n_episodes)):
             observation = env.reset()
             done = False
             score = 0
@@ -82,6 +147,7 @@ if __name__ == "__main__":
                 critic_loss.append(avg_loss[1])
                 total_loss.append(avg_loss[2])
 
+            score = 100*(score/env.labels.shape[0])
             score_history.append(score)
             avg_score = np.mean(score_history[-100:])
             avg_score_history.append(avg_score)
@@ -89,11 +155,22 @@ if __name__ == "__main__":
             if avg_score >= best_score:
                 best_score = avg_score
                 if not load_checkpoint:
-                    print("Saving Model")
+                    # print("Saving Model")
                     agent.save_models()
         
-        # print('episode', i, 'score %.1f' % score, 'avg_score %.1f' % avg_score, 'time_steps', n_steps, 'learning_steps', learn_iters)
+            # print('episode', i, 'score %.2f' % score, 'avg_score %.2f' % avg_score, 'time_steps', n_steps, 'learning_steps', learn_iters)
         
+        # TEST THE MODEL
+        test_score = 0
+        for i in range(env.images_test.shape[0]):
+            action, _, _ = agent.choose_action(env.images_test[i])
+            if action == env.labels_test[i]:
+                test_score += 1
+        test_accuracy = 100*(test_score/env.labels_test.shape[0])
+        print("training accuracy: {}".format(score))
+        print("testing accuracy: {}".format(test_accuracy))
+
+
         score_book[trial] = score_history
         actor_loss_book[trial] = actor_loss
         critic_loss_book[trial] = critic_loss
@@ -102,13 +179,13 @@ if __name__ == "__main__":
             
     print("\nStoring rewards data...")
     a = pd.DataFrame(score_book)
-    a.to_csv('data/PPO-LunarLander1000-rewards-test.csv')
+    a.to_csv('data/PPO-MNIST-rewards-train.csv')
     if not load_checkpoint:
         print("\nStoring losses...")
         b = pd.DataFrame(actor_loss_book)
-        b.to_csv('data/PPO-LunarLander2000-actor_loss.csv')
+        b.to_csv('data/PPO-MNIST-actor_loss.csv')
         c = pd.DataFrame(critic_loss_book)
-        c.to_csv('data/PPO-LunarLander2000-critic_loss.csv')
+        c.to_csv('data/PPO-MNIST-critic_loss.csv')
         d = pd.DataFrame(total_loss_book)
-        d.to_csv('data/PPO-LunarLander2000-total_loss.csv')
+        d.to_csv('data/PPO-MNIST-total_loss.csv')
     print("Experiment finshed")
--- ppo_torch.py
+++ ppo_torch.py
@@ -48,7 +48,7 @@ class PPOMemory:
         self.dones = []
 
 class ActorNetwork(nn.Module):
-    def __init__(self, n_actions, input_dims, alpha, fc1_dims=200, fc2_dims=200, chkpt_dir='tmp/ppo'):
+    def __init__(self, n_actions, input_dims, alpha, fc1_dims=256, fc2_dims=256, chkpt_dir='tmp/ppo'):
         super(ActorNetwork, self).__init__()
 
         self.checkpoint_file = os.path.join(chkpt_dir, 'actor_torch_ppo')
@@ -200,59 +200,6 @@ class Agent:
         total_loss = total_loss.cpu().detach().numpy()
 
         return actor_loss, critic_loss, total_loss
-    
-    def compute_grads(self):
-        for _ in range(self.n_epochs):
-            state_arr, action_arr, old_probs_arr, vals_arr,\
-            reward_arr, done_arr, batches = \
-                self.memory.generate_batches()
-            
-            values = vals_arr
-            advantage = np.zeros(len(reward_arr), dtype=np.float32)
-
-            for t in range(len(reward_arr) - 1):
-                discount = 1
-                a_t = 0
-                for k in range(t, len(reward_arr)-1):
-                    delta = reward_arr[k] + self.gamma*values[k+1]*(1-int(done_arr[k])) - values[k]
-                    a_t += discount*delta
-                    discount *= self.gamma*self.gae_lambda
-                advantage[t] = a_t
-            advantage = T.tensor(advantage).to(self.actor.device)
-
-
-            values = T.tensor(values).to(self.actor.device)
-            
-            for batch in batches:
-                states = T.tensor(state_arr[batch], dtype=T.float).to(self.actor.device)
-                states.requires_grad = True
-                old_probs = T.tensor(old_probs_arr[batch]).to(self.actor.device)
-                actions = T.tensor(action_arr[batch]).to(self.actor.device)
-
-                dist = self.actor(states)
-                critic_value = self.critic(states)
-
-                critic_value = T.squeeze(critic_value)
-
-                new_probs = dist.log_prob(actions)
-                prob_ratio = new_probs.exp()/ old_probs.exp()
-
-                weighted_probs = advantage[batch]*prob_ratio
-                weighted_clipped_probs = T.clamp(prob_ratio, 1-self.policy_clip, 1+self.policy_clip)*advantage[batch]
-                actor_loss = -T.min(weighted_probs, weighted_clipped_probs).mean()
-
-                returns = advantage[batch] + values[batch]
-                critic_loss = (returns-critic_value)**2
-                critic_loss = critic_loss.mean()
-
-                total_loss = actor_loss + 0.5*critic_loss
-                self.actor.optimizer.zero_grad()
-                self.critic.optimizer.zero_grad()
-                total_loss.backward()
-                data_grad = states.grad.data
-
-        self.memory.clear_memory()
-        return data_grad.mean(axis=0)
 
 
 
